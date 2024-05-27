014372dbab933c05841ac96c520108a93c3c5dd6
Illia <79321689+illiaxpwnz@users.noreply.github.com>
2024-05-27 16:28:03 +0300

    Update task5.py

--- task5.py
+++ task5.py
@@ -13,26 +13,25 @@ class Node:
 
 def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
-        graph.add_node(node.id, color=node.color, label=node.val)
+        graph.add_node(node.id, color=node.color, label=str(node.val))
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
             pos[node.left.id] = (l, y - 1)
-            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
             graph.add_edge(node.id, node.right.id)
             r = x + 1 / 2 ** layer
             pos[node.right.id] = (r, y - 1)
-            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
-    return graph
+            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
 
 def draw_tree(tree_root, title):
     tree = nx.DiGraph()
     pos = {tree_root.id: (0, 0)}
-    tree = add_edges(tree, tree_root, pos)
+    add_edges(tree, tree_root, pos)
 
-    colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+    colors = [node_data['color'] for node, node_data in tree.nodes(data=True)]
+    labels = {node: node_data['label'] for node, node_data in tree.nodes(data=True)}
 
     plt.figure(figsize=(8, 5))
     nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
@@ -40,11 +39,13 @@ def draw_tree(tree_root, title):
     plt.show()
 
 def generate_colors(n):
-    return ["#" + ''.join([format(int(x*255), '02x') for x in colorsys.hsv_to_rgb(i/n, 1, 1)]) for i in range(n)]
+    # Generate colors from dark to light in shades of blue
+    return ["#" + ''.join([format(int(x*255), '02x') for x in colorsys.hsv_to_rgb(0.6, 1, (i+1)/n)]) for i in range(n)]
 
 def dfs(node, step_colors, step_count, visited):
     if node:
         node.color = step_colors[step_count[0]]
+        print(f"DFS visiting node {node.val} with color {node.color}")
         visited.append(node)
         step_count[0] += 1
         dfs(node.left, step_colors, step_count, visited)
@@ -57,6 +58,7 @@ def bfs(root, step_colors, visited):
         node = queue.pop(0)
         if node:
             node.color = step_colors[step_count[0]]
+            print(f"BFS visiting node {node.val} with color {node.color}")
             visited.append(node)
             step_count[0] += 1
             queue.append(node.left)
@@ -91,7 +93,6 @@ draw_tree(heap_root, "DFS Traversal")
 heap_root = build_heap(array)
 step_colors = generate_colors(node_count)
 
-#Виводиться прінт і візуалізація паралельно щоб розуміти, на який обхід зараз дивимося
 print("BFS Traversal:")
 visited_bfs = []
 bfs(heap_root, step_colors, visited_bfs)
